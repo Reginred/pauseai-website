a2bd08cfa6428e7ad5f64546866f6db222461e23
Reginred <181634917+Reginred@users.noreply.github.com>
2024-09-27 18:22:43 +0200

    Run Google Analytics in web worker using Partytown

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { partytownVite } from '@builder.io/partytown/utils'
+import path from 'path'
 
 const config = {
 	server: {
 		port: 37572
 	},
-	plugins: [sveltekit()]
+	plugins: [
+		// Partytown first because SvelteKit copies from static to build in same hook (see below)
+		partytownVite({
+			// place in static as build is wiped by adapter which always runs last
+			dest: path.join(__dirname, 'static', '~partytown')
+		}),
+		sveltekit()
+	]
 }
 
 export default config
